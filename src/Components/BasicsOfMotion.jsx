5afd835f5f5218a663004d3a25c034a4fecce574
Abdirahaman Abdulaziz <abdirinoahash@gmail.com>
2023-11-06 14:22:06 +0300

    framer basics ✅

--- src/Components/BasicsOfMotion.jsx
+++ src/Components/BasicsOfMotion.jsx
@@ -1,7 +1,48 @@
-const BasicsOfMotion = () => {
-  return (
-    <div>BasicsOfMotion</div>
-  )
-}
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
-export default BasicsOfMotion
+const BasicsOfMotion = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div
+      style={{
+        display: "grid",
+        placeContent: "center",
+        height: "100vh",
+        gap: "0.8rem",
+      }}
+    >
+      <motion.button className="e-button" layout onClick={() => setIsVisible(!isVisible)}>
+        Show / Hide
+      </motion.button>
+      <AnimatePresence mode="popLayout">
+        {isVisible && (
+          <motion.div
+            initial={{
+              rotate: "0deg",
+              scale: 0,
+              y: 0
+            }}
+            animate={{
+              rotate: "180deg",
+              scale: 1,
+              y: [0, 180, -120, -130, 0]
+            }}
+            exit={{
+              rotate: "0deg",
+              scale: 0,
+              y: 0
+            }}
+            transition={{
+              duration: 1,
+              ease: "backInOut",
+            }}
+            style={{ width: "150px", height: "150px", background: "black", borderRadius: "6px" }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default BasicsOfMotion;
---
+++ src/Components/Gestures.jsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+
+const Gestures = () => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        placeContent: "center",
+        height: "100vh",
+        gap: "0.8rem",
+      }}
+    >
+      <motion.div>Gestures</motion.div>
+    </div>
+  );
+};
+
+export default Gestures;
